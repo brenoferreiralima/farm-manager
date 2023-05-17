533f4b8a25d09acf70c2f2193379c68929a88409
Breno Lima <brenoferreiralima@gmail.com>
2023-05-17 19:59:22 -0300

    rm "home/" path

--- core/core/urls.py
+++ core/core/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('home/', include('home.urls')),
     path('animals/', include('animals.urls')),
     path('admin/', admin.site.urls),
 ]
