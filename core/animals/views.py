6ccf81d34b94121872c805ebe494fba7f73b4426
Breno Lima <brenoferreiralima@gmail.com>
2023-05-18 21:15:53 -0300

    fix template names

--- core/animals/views.py
+++ core/animals/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 # list of all registered animals
 def index(request):
-    return render(request, "index.html")
+    return render(request, "animals_index.html")
 
 
 def detail(request):
